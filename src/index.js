4bffe169c2be8088ede78e979376c416fb198358
Phipat Limprasutr <phipat.limprasutr@outlook.com>
2020-10-12 16:58:00 +0700

    Finish Jumbotron + basic styling @1.00.00

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from "react";
 import { render } from "react-dom";
-import App from "./app";
+import App from "./App";
+import {GlobalStyles} from './global-styles'
+import 'normalize.css'
 
-render(<App />, document.getElementById("root"));
+render( <>
+        <GlobalStyles />
+        <App />
+        </>,
+        document.getElementById("root"));
