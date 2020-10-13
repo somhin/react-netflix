a6211cf46045152e303b6adbaed7a8b8c6dca8bc
Phipat Limprasutr <phipat.limprasutr@outlook.com>
2020-10-13 20:10:04 +0700

    Finish Footer

--- src/components/Jumbotron/index.js
+++ src/components/Jumbotron/index.js
@@ -1,26 +1,26 @@
-import React from 'react'
-import { Item, Inner, Container, Pane, Title, SubTitle, Image } from './styles/Jumbotron'
+import React from 'react';
+import { Item, Inner, Container, Pane, Title, SubTitle, Image } from './styles/Jumbotron';
 
-export default function Jumbotron({ children, direction = "row", ...restProps }) {
-return (<Item direction={direction}>
-           <Inner>
-                {children}
-           </Inner>
-        </Item>);
+export default function Jumbotron({ children, direction = 'row', ...restProps }) {
+  return (
+    <Item {...restProps}>
+      <Inner direction={direction}>{children}</Inner>
+    </Item>
+  );
 }
 
-Jumbotron.Container = function JumbotronContainer({ children, ...restProps}) {
-    return <Container { ...restProps }>{children}</Container>;
-}
-Jumbotron.Pane = function JumbotronPane({ children, ...restProps}) {
-    return <Pane { ...restProps }>{children}</Pane>;
-}
-Jumbotron.Title = function JumbotronTitle({ children, ...restProps}) {
-    return <Title { ...restProps }>{children}</Title>;
-}
-Jumbotron.SubTitle = function JumbotronSubTitle({ children, ...restProps}) {
-    return <SubTitle { ...restProps }>{children}</SubTitle>;
-}
+Jumbotron.Container = function JumbotronContainer({ children, ...restProps }) {
+  return <Container {...restProps}>{children}</Container>;
+};
+Jumbotron.Pane = function JumbotronPane({ children, ...restProps }) {
+  return <Pane {...restProps}>{children}</Pane>;
+};
+Jumbotron.Title = function JumbotronTitle({ children, ...restProps }) {
+  return <Title {...restProps}>{children}</Title>;
+};
+Jumbotron.SubTitle = function JumbotronSubTitle({ children, ...restProps }) {
+  return <SubTitle {...restProps}>{children}</SubTitle>;
+};
 Jumbotron.Image = function JumbotronImage({ ...restProps }) {
-    return <Image {...restProps} />;
-}
+  return <Image {...restProps} />;
+};
